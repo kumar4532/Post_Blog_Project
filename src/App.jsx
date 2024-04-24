969a24a06ca051396a6064ce8a46a69637b3fef0
kumar4532 <krantikumar2001@gmail.com>
2024-04-24 16:02:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  console.log(appwriteUrl);
+  return (
+    <>
+      <h1>A blog with appwrite</h1>
+    </>
+  )
+}
+
+export default App
