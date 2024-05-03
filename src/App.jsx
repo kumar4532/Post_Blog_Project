609097ede84e222787f6dbdf2e62a4f8550f43f3
kumar4532 <krantikumar2001@gmail.com>
2024-05-03 13:34:40 +0530

    add components and config files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice.js"
+import {Header, Footer} from "./components/index.js"
 
 function App() {
 
-  console.log(appwriteUrl);
-  return (
-    <>
-      <h1>A blog with appwrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        todo  {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App
