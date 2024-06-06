c3e1b6e1ef986a16ea6630a5b921182f995a96d9
kumar4532 <krantikumar2001@gmail.com>
2024-06-06 19:40:20 +0530

    update api key

--- src/appwrite/user.js
+++ src/appwrite/user.js
@@ -10,7 +10,7 @@ export class AuthService {
         this.client
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId)
-            .setKey('a8cc4f7f2a11cb6b19ad4a2fb903e798007ced4c6284d8843311e342e01bb44ae4de8000b63c5fcca78dde1602f99fe8ea3dc6bdf72233c65e0d3af99bdc873ed6845e2e436ec0fd2eb78ec96074acd48841f6bfead56ec0bae79454fe1f74e7314da4f78c9ebfa33788e9edba1317f0aa8895ce0ab303144c95120f3c018ae0');
+            .setKey(conf.appwriteSecretKey);
 
         this.users = new Users(this.client);
     }
